9b2bba453041a1bd5883be798cba61e2c054d6d5
anirudh-s1308 <anirudhs1308@gmail.com>
2026-02-12 12:18:59 +0530

    vcard qr generator shcema route and vcard endpoint add

--- backend/app/main.py
+++ backend/app/main.py
@@ -5,6 +5,7 @@ from app.services.qr import generate_qr_mail, generate_qr_wifi
 from app.api.url import router as url_router
 from app.api.mail import router as mail_router
 from app.api.wifi import router as wifi_router
+from app.api.vcard import router as vcard_router
 app = FastAPI()
 
 app.include_router(url_router, prefix="/api/v1/qr")
@@ -12,6 +13,8 @@ app.include_router(url_router, prefix="/api/v1/qr")
 app.include_router(mail_router,prefix="/api/v1/qr")
 
 app.include_router(wifi_router, prefix="/api/v1/qr")
+
+app.include_router(vcard_router , prefix="/api/v1/qr")
 
 
 @app.get("/healthz")
