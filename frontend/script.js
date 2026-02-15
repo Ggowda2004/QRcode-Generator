4ddd10e7d29840a2f85de9df7d79b175bf42df3d
GANGADHAR GOWDA K M <kmgangadhargowda2004@gmail.com>
2026-02-15 15:50:25 +0530

    Fix BASE_URL string formatting in script.js

--- frontend/script.js
+++ frontend/script.js
@@ -7,7 +7,7 @@ const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
 const downloadBtn = document.getElementById("downloadBtn");
 
-const BASE_URL = https://qrcode-generator-18jm.onrender.com/api/v1/qr;
+const BASE_URL = "https://qrcode-generator-18jm.onrender.com/api/v1/qr";
 
 /* SWITCH TYPE */
 function setType(event, type) {
